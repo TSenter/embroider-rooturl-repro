4b87d49f43f4baf0e96e205aa2964039db77ba0c
TSenter <tyler.senter.427@gmail.com>
2024-09-16 13:22:27 -0400

    chore: Use Embroider for build in test-app-embroider

--- apps/test-app-embroider/ember-cli-build.js
+++ apps/test-app-embroider/ember-cli-build.js
@@ -5,11 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
-    autoImport: {
-      watchDependencies: ['embroider-rooturl-repro'],
-    },
+    // autoImport: {
+    //   watchDependencies: ['embroider-rooturl-repro'],
+    // },
   });
 
-  const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack);
 };
